3e504e00663b07930452f1e64cf00765885cf47c
peacemeda <55708580+peacemeda@users.noreply.github.com>
2025-11-25 19:16:52 -0800

    Add Day 2 notes on 30Day-of-python

---
+++ Day-2.py
@@ -0,0 +1,9 @@
+  Day 2
+Buildt in function
+  print() 				min()
+  len()				max()
+  type()				sum([ ])
+  help(‘Keywords’)
+
+Declaring Multiple Variable in a Line
+  firstName, lastName, age, is_cold = ‘Meda’, ‘Mesh’, 25, True
