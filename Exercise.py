0f7f77a4aa753825caa0b9182f16ffa65c579822
Medhanie Meshesha <peacemedhane@gmail.com>
2026-01-04 22:30:18 -0800

    continue exercise 5

--- Exercise.py
+++ Exercise.py
@@ -17,6 +17,12 @@ days, language = 30, 'python'
 
 #Exercise 2
 
+"""
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+"""
+
 #1 data type
 print(type(10))
 #2
@@ -71,6 +77,12 @@ print(f'The first Name is {first_name} Last Name is {last_name} country is {coun
 help('keywords')
 
 #Exercise 3
+
+"""
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+"""
 #1
 age = 20
 #2
@@ -184,21 +196,26 @@ print(f'You have lived for {year_per_second} seconds.')
 for num in range(1, 6):
   print(num, 1 , num**2, num**3)
 
-  # Exercise 4
+# Exercise 4
 
-  # 1 Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
-  w = ['Thirty', 'Days', 'Of', 'Python']
-  r = ' '.join(w)
-  print(r)
+"""
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+"""
+# 1 Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+w = ['Thirty', 'Days', 'Of', 'Python']
+r = ' '.join(w)
+print(r)
 
-  #2 Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+#2 Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
 
-  w = ['Coding', 'For' , 'All']
-  r = ' '.join(w)
-  print(r)
+w = ['Coding', 'For' , 'All']
+r = ' '.join(w)
+print(r)
 
-  #3 Declare a variable named company and assign it to an initial value "Coding For All".
-  company = 'Coding For All'
+#3 Declare a variable named company and assign it to an initial value "Coding For All".
+company = 'Coding For All'
 
   #4 Print the variable company using print().
 print(company)
@@ -348,3 +365,124 @@ print(f'{8} / {6} = {8/6:.2f}')
 print(f'{8} % {6} = {8%6}')
 print(f'{8} // {6} = {8//6}')
 print(f'{8} ** {6} = {8**6}')
+
+
+# Exercise 5
+
+"""
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+"""
+
+# Declare an empty list
+lst = list()
+lst1 = []
+
+#1 Declare a list with more than 5 items
+lst = [1,2,'age','item',True,0]
+
+#2 Find the length of your list
+len(lst)
+
+#3 Get the first item, the middle item and the last item of the list
+print(lst[0])
+print(lst[3])
+print(lst[-1])
+
+#4Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixted_data_types = ['medhanie',40, 1.71,'is_married','4906 w 63rd st, Los Angeles']
+#5 Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companie = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle','Amazon']
+#6 Print the list using print()
+print(it_companie)
+#7 Print the number of companies in the list
+print(len(it_companie))
+#8 Print the first, middle and last company
+print(it_companie[0])
+print(it_companie[3])
+print(it_companie[-1])
+#9 Print the number of companies in the list
+print(len(it_companie))
+
+#10 Print the list after modifying one of the companies
+print(it_companie)
+
+#11Add an IT company to it_companies
+it_companie.append('uber')
+
+#12 Insert an IT company in the middle of the companies list
+it_companie.insert(3,'Grub hub')
+
+#13 Change one of the it_companies names to uppercase (IBM excluded!)
+it_companie[2] = it_companie
+
+#14 Join the it_companies with a string '#;  '
+resual = '#; '.join(it_companie)
+
+#15 Check if a certain company exists in the it_companies list.
+print('Apple' in it_companie)
+
+#16 Sort the list using sort() method
+it_companie.sort()
+
+#17 Reverse the list in descending order using reverse() method
+it_companie.reverse()
+
+#18 Slice out the first 3 companies from the list
+print(it_companie[:3])
+
+#19 Slice out the last 3 companies from the list
+print(it_companie[-3:])
+
+#20 Slice out the middle IT company or companies from the list
+
+#21 Remove the first IT company from the list
+it_companie.remove('Amazon')
+it_companie.pop()
+
+#22 Remove the middle IT company or companies from the list
+length = len(it_companie)
+
+if length % 2 == 1:
+    # odd number of companies → one middle
+    middle = it_companie[length//2 : length//2 + 1]
+else:
+    # even number of companies → two middle
+    middle = it_companie[length//2 - 1 : length//2 + 1]
+
+print(middle)
+
+#23 Remove the last IT company from the list
+print(it_companie.pop(-1))
+
+#24 Remove all IT companies from the list
+print(it_companie.clear())
+
+#25Destroy the IT companies list
+del it_companie
+
+
+# 26 Join the following lists:
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+
+front_end.extend(back_end)
+
+# 27 After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack, then insert Python and SQL after Redux.
+
+"""
+Exercises: Level 2
+The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+Sort the list and find the min and max age
+Add the min age and the max age again to the list
+Find the median age (one middle item or two middle items divided by two)
+Find the average age (sum of all items divided by their number )
+Find the range of the ages (max minus min)
+Compare the value of (min - average) and (max - average), use abs() method
+Find the middle country(ies) in the countries list
+Divide the countries list into two equal lists if it is even if not one more country for the first half.
+['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+"""
