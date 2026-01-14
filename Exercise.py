c3a26e24c8bd3855d1394fe6ffa5429fa1eb2a25
Medhanie Meshesha <peacemedhane@gmail.com>
2026-01-13 22:42:27 -0800

    Exercise updated

--- Exercise.py
+++ Exercise.py
@@ -740,12 +740,12 @@ if 'skills' in person:
         print(middle)
 
  #8 * Check if the person dictionary has skills key, if so check if the person has 'Python' skill and print out the result.
- if 'Python' in skills:
+if 'Python' in skills:
     print(f'The person has the {'Python'} skill')
 
  #9 * If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB, print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'), else print('unknown title') - for more accurate results more conditions can be nested!
 
- skills = person.get('skills', [])
+skills = person.get('skills', [])
 skill_set = set(skills)
 
 if skill_set == {'JavaScript', 'React'}:
@@ -760,3 +760,89 @@ else:
 #10 * If the person is married and if he lives in Finland, print the information in the following format:
 if person.get('is_married') and person.get('country') == 'Finland':
     print(f'The person first name is {person['first_name']} last name is {person['last_name']}')
+
+#///////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////
+
+#Exercise 10
+
+#1 Iterate 0 to 10 using for loop, do the same using while loop.
+count = 0
+while count < 11: 
+    print(count)
+    count += 1
+
+for i in range(11):
+    print(i)
+#2 Iterate 10 to 0 using for loop, do the same using while loop.
+for i in range(10,-1,-1):
+    print(i)
+
+count = 10
+while count > -1:
+    print(count)
+    count -= 1
+
+#3 Write a loop that makes seven calls to print(), so we get on the output the following triangle:
+for i in range(7):
+   for j in range(i+1):
+       print('#',end='')
+   print()
+
+#4 Use nested loops to create the following:
+for i in range(1, 9):
+    for j in range(1, 9):
+        print('#', end=' ')
+    print()
+
+#5 Print the following pattern:
+'''
+0 x 0 = 0
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
+4 x 4 = 16
+5 x 5 = 25
+6 x 6 = 36
+7 x 7 = 49
+8 x 8 = 64
+9 x 9 = 81
+10 x 10 = 100
+'''
+
+for num in range(11):
+    print(f'{num} x {num} = {num * num}')
+
+#6 Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
+lst = ['Python', 'Numpy','Pandas','Django', 'Flask']
+for lst in lst:
+    print(lst)
+
+#7 Use for loop to iterate from 0 to 100 and print only even numbers
+for num in range(0, 101):
+    if num % 2 == 0:
+        print(num, end=' ')
+    continue
+#8 Use for loop to iterate from 0 to 100 and print only odd numbers
+for num in range(0, 101):
+    if num % 2 == 1:
+        print(num, end=' ')
+    continue
+
+#9 Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+sum = 0
+for num in range(101):
+    sum += num
+print(f'The sum of all numbers is {sum}')
+
+# 10 Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+even_sum = 0
+odd_sum = 0
+
+for num in range (101):
+    if num % 2 == 0:
+        even_sum += num
+    else:
+        odd_sum += num
+print(f'The sum of all even is {even_sum}; and sum of all odd is {odd_sum}.')
